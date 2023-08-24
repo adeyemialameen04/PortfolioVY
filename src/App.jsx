11e0b1b669fd8b835ee00e0b11856c170866f9ac
Al Ameen Adeyemi <adeyemialameen04@gmail.com>
2023-08-24 01:12:37 +0100

    added theme switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { createContext, useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 
+export const ThemeContext = createContext(null);
+
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const handleToggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   return (
-    <>
-      <Navbar />
-      <h1>Hello</h1>
-    </>
+    <ThemeContext.Provider value={{ theme, handleToggleTheme }}>
+      <div id={theme === "light" ? "light" : "dark"}>
+        <Navbar />
+        <h1>Hello</h1>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
